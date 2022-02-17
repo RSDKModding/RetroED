0ea1450f735ec6012c6ac054472d5d790735c45f
Chuli <rmgrichdeveloper@gmail.com>
2022-02-17 13:27:36 -0600

    OH MY GOD IM FUCKING STUPID

--- RetroEDv2/versiongen.py
+++ RetroEDv2/versiongen.py
@@ -1,27 +1,33 @@
 from datetime import datetime
-import pytz, sys
+import pytz
+import sys
 
 buildadd = "dev"
 buildtype = 0
 
 try:
     buildadd = sys.argv[1]
-except: pass
-if buildadd != "dev":
-    if len(buildadd) >= 6: #autobuild commit hash
-        buildtype = 2
-        buildadd = buildadd[:6]
-    else: buildtype = 1
+except:
+    pass
+
+if len(buildadd) >= 7:  # autobuild commit hash
+    buildtype = 2
+    buildadd = "-" + buildadd[:7]
+elif buildadd != "dev":
+    buildtype = 1
+else:
+    buildadd = "-dev"
+
 
 time = datetime.now(pytz.timezone("US/Eastern"))
-s = time.strftime("v%Y.%m.%d-") + buildadd
+s = time.strftime("v%Y.%m.%d") + buildadd
 
 LINES = [
     "#ifndef VERSION_H\n",
     "#define VERSION_H\n",
-   f"#define RE_VERSION    (\"{s}\")\n",
-   f"#define RE_BUILD_TYPE ({buildtype})\n",
-    "#endif" 
+    f"#define RE_VERSION    (\"{s}\")\n",
+    f"#define RE_BUILD_TYPE ({buildtype})\n",
+    "#endif"
 ]
 
 open("version.hpp", "w").writelines(LINES)
